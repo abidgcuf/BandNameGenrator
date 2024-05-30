b16ac159f79c817aa96af546973e0605399acf01
Abid Ali <seabidali@gmail.com>
2024-05-30 14:45:22 +0400

    Add files via upload

---
+++ project1/main.py
@@ -0,0 +1,7 @@
+print("Welcom to Band Generator!")
+
+city = input(" Plz Input your city name in which you grow up ")
+
+pet = input(" Plz Input your Pet name ")
+
+print(" Yor band name could be " + city + " " + pet)
